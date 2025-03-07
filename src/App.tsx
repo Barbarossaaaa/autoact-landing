a12b12c718f2fd6bbcbaadd30ac2c228e9382617
Jainam Shah <105748448+JainamShah28@users.noreply.github.com>
2025-03-07 16:50:37 +0530

    update: selection background color

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import Home from './pages/Home';
 
 const App: React.FC = () => {
     return (
-        <div className="selection:bg-primary-600 selection:text-white flex flex-col font-inter h-full w-full items-center bg-[#F5F5FA]">
+        <div className="selection:bg-blue-600 selection:text-white flex flex-col font-inter h-full w-full items-center bg-[#F5F5FA]">
             <header className="flex w-full justify-center h-[44px] lg:h-[62px] xl:h-[62px] mac:h-[66px] 2xl:h-[88px] bg-[#131951] bg-opacity-[24%] backdrop-blur-lg z-50 fixed pointer-events-none">
                 <div className="max-w-[440px] sm:max-w-[488px] md:max-w-[664px] lg:max-w-[980px] xl:max-w-[1140px] mac:max-w-[1140px] 2xl:max-w-[1520px] flex w-full h-full items-center">
                     <Navbar />
